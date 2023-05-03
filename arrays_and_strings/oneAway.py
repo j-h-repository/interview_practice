9605d9483b9da68634e80f4994cbea758f0ea1be
sailoryoung <jordonharris@protonmail.com>
2023-05-03 09:42:09 -0400

    one change challenge

--- arrays_and_strings/oneAway.py
+++ arrays_and_strings/oneAway.py
@@ -10,3 +10,25 @@ pales, pale -> true
 pale, bale -> true 
 pale, bake -> false
 '''
+
+
+def checkEdits(string):
+    before = string.split(", ")[0]
+    after = string.split(", ")[1]
+
+    
+    changes =0#store change detection as an integer starting at 0
+    oneOrLessChange=True#default False
+    #grab second string
+    for char in range(len(after)):#interate through
+        if(after[char] not in before):#on each iteration, check if char in string2 is not included in string1
+            changes=changes+1# increment changes
+            if changes>1:#if more than one change is detected
+                oneOrLessChange = False
+                return ('one change or less: ' + str(oneOrLessChange))#return "1 or less change" to false
+    #return 
+    return ('one change or less: ' + str(oneOrLessChange))
+
+
+result = checkEdits('pale, bake')
+print(result)
